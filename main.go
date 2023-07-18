24a68bdac532136e360097609a7dab361129558f
Jonny Lezcano <jlezcanoa@ext.falabella.cl>
2023-07-18 12:06:50 -0400

    feat: add main to start

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/programmers18/go-finance/api"
+	db "github.com/programmers18/go-finance/db/sqlc"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
+	serverAddress = "0.0.0.0:8000"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start api: ", err)
+	}
+}
